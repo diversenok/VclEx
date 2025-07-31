9f4f5bfb721a6500a4a800ca6782c6cd9a94b82f
diversenok <diversenok@gmail.com>
2025-07-31 02:39:52 +0200

    Fix FormStyle desync on external changes

--- VclEx.Form.pas
+++ VclEx.Form.pas
@@ -1,5 +1,9 @@
 unit VclEx.Form;
 
+{
+  This module introduces improvement to the form component.
+}
+
 interface
 
 uses
@@ -11,6 +15,7 @@ type
     const idOnTop = 10001;
     procedure WMSysCommand(var Message: TWMSysCommand); message WM_SYSCOMMAND;
     procedure WMInitMenuPopup(var Message: TWMInitMenuPopup); message WM_INITMENUPOPUP;
+    procedure WMWindowPosChanged(var Message: TWMWindowPosChanged); message WM_WINDOWPOSCHANGED;
   protected
     procedure DoClose(var Action: TCloseAction); override;
     procedure DoCreate; override;
@@ -19,7 +24,14 @@ type
     function ShowModal: Integer; override;
   end;
 
+function IsWindowTopmost(Handle: HWND): Boolean;
+
 implementation
+
+function IsWindowTopmost;
+begin
+  Result := GetWindowLongPtrW(Handle, GWL_EXSTYLE) and WS_EX_TOPMOST <> 0
+end;
 
 { TFormEx }
 
@@ -32,16 +44,19 @@ end;
 procedure TFormEx.DoCreate;
 begin
   inherited;
-  InsertMenu(GetSystemMenu(Handle, False), 0, MF_STRING, idOnTop,
-    'Stay On &Top');
+
+  // Add an item to toggle stay-on-top from the system menu
+  if FormStyle in [fsNormal, fsStayOnTop] then
+    InsertMenu(GetSystemMenu(Handle, False), 0, MF_STRING, idOnTop,
+      'Stay On &Top');
 end;
 
 procedure TFormEx.DoShow;
 begin
-  // Inherit stay-on-top style to prevent children hiding behind a parent
-  if Owner is TForm then
-    if TForm(Owner).FormStyle = fsStayOnTop then
-      FormStyle := fsStayOnTop;
+  // Inherit stay-on-top style to prevent children from hiding behind the parent
+  if (FormStyle = fsNormal) and (Owner is TWinControl) and
+    IsWindowTopmost(TWinControl(Owner).Handle) then
+    FormStyle := fsStayOnTop;
 end;
 
 function TFormEx.ShowModal;
@@ -53,37 +68,52 @@ begin
 end;
 
 procedure TFormEx.WMInitMenuPopup;
+const
+  STATE: array [Boolean] of Cardinal = (MF_UNCHECKED, MF_CHECKED);
 begin
-  if not Message.SystemMenu then
-    Exit;
+  // Update the check state of our stay-on-top menu item
+  if Message.SystemMenu then
+    CheckMenuItem(Message.MenuPopup, idOnTop, STATE[IsWindowTopmost(Handle)]);
 
-  // Update state of our stay-on-top menu item
-  if FormStyle = fsStayOnTop then
-    CheckMenuItem(Message.MenuPopup, idOnTop, MF_CHECKED)
-  else
-    CheckMenuItem(Message.MenuPopup, idOnTop, MF_UNCHECKED);
+  inherited;
 end;
 
 procedure TFormEx.WMSysCommand;
-var
-  hSysMenu: HMENU;
 begin
+  // Toggle the stay-on-top state from the menu
   if Message.CmdType = idOnTop then
-  begin
-    hSysMenu := GetSystemMenu(Handle, False);
-    if FormStyle = fsNormal then
-    begin
-      FormStyle := fsStayOnTop;
-      CheckMenuItem(hSysMenu, idOnTop, MF_CHECKED);
+    case FormStyle of
+      fsNormal:    FormStyle := fsStayOnTop;
+      fsStayOnTop: FormStyle := fsNormal;
     end
-    else
-    begin
-      FormStyle := fsNormal;
-      CheckMenuItem(hSysMenu, idOnTop, MF_UNCHECKED);
-    end;
-  end
   else
     inherited;
+end;
+
+procedure TFormEx.WMWindowPosChanged;
+begin
+  inherited;
+
+  if Message.WindowPos.flags and SWP_NOZORDER = 0 then
+  begin
+    // FormStyle can desync from the actual state if an external caller changes
+    // it. Detect and fix it here.
+    case FormStyle of
+      fsNormal:
+        if IsWindowTopmost(Handle) then
+          FormStyle := fsStayOnTop;
+
+      fsStayOnTop:
+        if not IsWindowTopmost(Handle) then
+          FormStyle := fsNormal;
+    end;
+
+    // Also, if any window becomes topmost, make sure the application
+    // window does as well, so that dialogs can inherit the style from it.
+    if FormStyle = fsStayOnTop then
+      SetWindowPos(Application.Handle, HWND_TOPMOST, 0, 0, 0, 0,
+        SWP_NOMOVE or SWP_NOSIZE or SWP_NOACTIVATE or SWP_NOOWNERZORDER);
+  end;
 end;
 
 end.
